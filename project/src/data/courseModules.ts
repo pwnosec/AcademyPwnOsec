26f35bcf1a259f630f579d9e6289f9e31106c593
PWNOSEC <digitaltechnologiesltdptpwnose@gmail.com>
2024-12-25 19:51:44 +0700

    Delete project directory
    Signed-off-by: PWNOSEC <digitaltechnologiesltdptpwnose@gmail.com>

--- project/src/data/courseModules.ts
+++
@@ -1,2 +0,0 @@
-// This file is now just a re-export of all modules
-export * from './modules';
--- project/src/data/modules/csrf.ts
+++
@@ -1,3 +0,0 @@
-// This file is now deprecated as we're using the modular structure
-// All CSRF module exports are now handled by ./csrf/index.ts
-export * from './csrf/index';
